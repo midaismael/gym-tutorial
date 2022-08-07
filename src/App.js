0b8e43db096db99eae3723452aed9e682a0915e5
Ahmed Ismael <midaismael@gmail.com>
2022-08-07 16:22:13 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
-
+import Header from "./Components/Header";
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
     </div>
   );
 }
